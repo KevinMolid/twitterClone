d3e3076fd5ee97b41bf4f0ff910c3b07d4070de9
Kevin Molid <kevinmolid@gmail.com>
2023-10-29 17:39:12 +0100

    Following course

---
+++ index.js
@@ -0,0 +1,120 @@
+/* Imports */
+
+import { tweetsData } from './data.js'
+
+/* Variables */
+
+const tweetInput = document.getElementById('tweet-input')
+const tweetBtn = document.getElementById('tweet-btn')
+
+/* Event listeners */
+
+document.addEventListener('click', function(e){
+    if(e.target.dataset.reply){
+        console.log(e.target.dataset.reply)
+    } 
+    else if(e.target.dataset.like) {
+        handleLikeClick(e.target.dataset.like)
+    } 
+    else if(e.target.dataset.retweet) {
+        handleRetweetClick(e.target.dataset.retweet)
+    }
+})
+
+tweetBtn.addEventListener('click', function(){
+    console.log(tweetInput.value)
+})
+
+/* Functions */
+
+function handleLikeClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+
+    if(targetTweetObj.isLiked){
+        targetTweetObj.likes--
+    }
+    else{
+        targetTweetObj.likes++
+    }
+
+    targetTweetObj.isLiked = !targetTweetObj.isLiked
+    render()
+}
+
+function handleRetweetClick(tweetId){
+    const targetTweetObj = tweetsData.filter(function(tweet){
+        return tweet.uuid === tweetId
+    })[0]
+    
+    if(targetTweetObj.isRetweeted){
+        targetTweetObj.retweets--
+    }
+    else{
+        targetTweetObj.retweets++
+    }
+    targetTweetObj.isRetweeted = !targetTweetObj.isRetweeted
+    render()
+}
+
+function getFeedHtml(){
+    let feedHtml = ``
+    
+    tweetsData.forEach(function(tweet){
+
+        let likeIconClass = ''
+        let retweetIconClass = ''
+
+        if(tweet.isLiked){
+            likeIconClass = 'liked'
+        }
+
+        if(tweet.isRetweeted){
+            retweetIconClass = 'retweeted'
+        }
+
+        if(tweet.replies.length > 0){
+            console.log(tweet.uuid)
+        }
+
+        feedHtml += `
+<div class="tweet">
+    <div class="tweet-inner">
+        <img src="${tweet.profilePic}" class="profile-pic">
+        <div>
+            <p class="handle">${tweet.handle}</p>
+            <p class="tweet-text">${tweet.tweetText}</p>
+            <div class="tweet-details">
+                <span class="tweet-detail">
+                    <i class="fa-regular fa-comment-dots" 
+                    data-reply="${tweet.uuid}"></i>
+                    ${tweet.replies.length}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-heart ${likeIconClass}" 
+                    data-like="${tweet.uuid}"></i>
+                    ${tweet.likes}
+                </span>
+                <span class="tweet-detail">
+                    <i class="fa-solid fa-retweet ${retweetIconClass}" 
+                    data-retweet="${tweet.uuid}"></i>
+                    ${tweet.retweets}
+                </span>
+            </div>   
+        </div>            
+    </div>
+</div>
+`
+   })
+   return feedHtml 
+}
+
+function render(){
+    document.getElementById('feed').innerHTML = getFeedHtml()
+}
+
+/* Run */
+
+render()
+
